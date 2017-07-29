154bd08de6104c2e9a0f80f978f2af8be4440eba
Kitterini <p.t.filipkowski@gmail.com>
2017-07-29 20:52:07 +0200

    Add files via upload

---
+++ Assets/playerScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour {
+
+    private float maxSpeed = 12f;
+
+    private Transform trans;
+    private Rigidbody2D rg2d;
+
+    bool grounded = false;
+
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+
+    // Use this for initialization
+    void Start () {
+        trans = GetComponent<Transform>(); //pobieranie cech komponentu Transform
+        rg2d = GetComponent<Rigidbody2D>(); //pobieranie cech komponentu Rigidbody2D
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // Transformacje położenie w zależności od naciśniętego klawisza
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    trans.Translate(-0.25f, 0, 0);
+        //}
+        //else if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    trans.Translate(0.25f, 0, 0);
+        //}
+        //else if (Input.GetKey(KeyCode.UpArrow))
+        //{
+        //    trans.Translate(0, 0.25f, 0);
+        //}
+
+        //rotacja postaci, jezeli jest przekrzywiona
+        ////Do poprawy poniewaz obecnie postac drga -.- przy malych bardzo kątach, mniejszych niz kat obrotu (grrr)
+        //if (trans.rotation.z > 0)
+        //{
+        //    trans.Rotate(0, 0, -2);
+        //}
+        //else if(trans.rotation.z < 0)
+        //{
+        //    trans.Rotate(0, 0, 2);
+        //}
+        if (grounded && Input.GetKeyDown(KeyCode.Space)) //skok przy użyciu fizyki
+        {
+            rg2d.AddForce(new Vector2(0, 550.0f));
+        }
+
+    }
+
+    private void FixedUpdate() //używajmy fizyki do ruchu, jest bardziej naturalnie, w tej funkcji zamieszcza się większość rzeczy związanych z fizyką
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+
+        float move = Input.GetAxis("Horizontal");
+        rg2d.velocity = new Vector2(move * maxSpeed, rg2d.velocity.y);
+    }
+
+
+}
---
+++ Assets/portalApearing.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class portalApearing : MonoBehaviour {
+
+    private Transform trans;
+    public GameObject portal;
+    public LayerMask isItWall;
+
+    public portalShooting player;
+    enum Color{BLUE, ORANGE};
+    private Color color;
+    
+    private string portalName;
+    private Vector2 startPosition;
+
+   	// Use this for initialization
+	void Start () {
+        trans = GetComponent<Transform>();
+        Vector2 startPosition = new Vector2(trans.position.x, trans.position.y); // zapisanie początkowego położenia kuli
+        if (trans.gameObject.name == "blueProjectile") //w zaleznosci od koloru pocisku, jest zdeterminowana nazwa portalu
+            {
+                color = Color.BLUE;
+                portalName = "bluePortal";
+            }
+        else if(trans.gameObject.name == "orangeProjectile")
+            {
+                color = Color.ORANGE;
+                portalName = "orangePortal";
+            }
+   
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Vector2 velocity = GetComponent<Rigidbody2D>().velocity;
+
+        if (IsInLayerMask(col.gameObject.layer, isItWall.value))
+        {
+            if (color == Color.BLUE)//usuniecie starego portalu (w zaleznosci, jakiego koloru tworzony jest nowy)
+            {
+                if (player.blueOldPortal != null)
+                    Destroy(player.blueOldPortal);
+            }
+            else if (color == Color.ORANGE)
+            {
+                if (player.orangeOldPortal != null)
+                    Destroy(player.orangeOldPortal);
+            }
+
+            Quaternion rotation = col.gameObject.GetComponent<Transform>().rotation; //pobieranie rotacji obiektu z ktorym zaszla kolizjia (sciana)
+            //tworzenie kopii portalu, z uwzglednieniem pozycji w ktorej sie styknal ze scianal i rotacja w jakiej jest sciana (przyjmuje taką samą rotacje)
+            GameObject newPortal = (GameObject)Instantiate(portal, trans.position, rotation);
+            
+            Vector3 position = col.gameObject.GetComponent<Transform>().position; //pobieranie pozycji obiektu z ktorym zaszla kolizjia (sciana)
+            //wykrywanie w jakim kierunku leciała kula względem ściany z którą się zderzyła
+            if (rotation.z == 0)
+            {
+                if (startPosition.x > position.x)
+                {
+                    Debug.Log("Kula leci w lewo!");
+                }
+                else if(startPosition.x < position.x)
+                {
+                    Debug.Log("Kuls leci w prawo!");
+                }
+            }
+            else
+            {
+                if (startPosition.y > position.y)
+                {
+                    Debug.Log("Kula leci w dół!");
+                }
+                else if(startPosition.y < position.y)
+                {
+                    Debug.Log("Kula leci w górę!");
+                }
+            }
+
+            newPortal.name = portalName; // przypisanie nazwy skopiowanemu portalowi
+            if (color == Color.BLUE) //przypisanie nowego portalu do posiadanej przez gracza referencji portalu
+                player.blueOldPortal = newPortal;
+            else if (color == Color.ORANGE)
+                player.orangeOldPortal = newPortal;
+            
+            DestroyObject(trans.gameObject); // usuwanie pocisku
+        }        
+    }
+
+    public static bool IsInLayerMask(int layer, LayerMask layermask) //sprawdzenie czy dany 'layer' znajduje się w tych podanych do skryptu ('layermask')
+    {
+        return layermask == (layermask | (1 << layer));
+    }
+}
